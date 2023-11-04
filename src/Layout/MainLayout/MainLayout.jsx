efb98a7b1358757ac8f8d96ccd88cd13ff63a94c
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2023-11-04 23:12:02 +0600

    navbar create kore felam

--- src/Layout/MainLayout/MainLayout.jsx
+++ src/Layout/MainLayout/MainLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../../conponents/Navbar/Navbar";
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className="w-full max-w-[1200px] mx-auto">
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );

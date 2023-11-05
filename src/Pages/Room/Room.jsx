8d8e3b16cd6c776cf16620b6db51a26d98fca1ae
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2023-11-06 00:31:02 +0600

    room page a filter er kaj completed

--- src/Pages/Room/Room.jsx
+++ src/Pages/Room/Room.jsx
@@ -8,15 +8,11 @@ const Room = () => {
   
     return (
         <div>
-             <h1 className="text-center text-black text-5xl font-bold py-4">Explore Our Rooms</h1>
+             <h1 className="text-center text-black text-5xl font-bold py-10 ">Explore Our Rooms</h1>
              <div>
                 <FilterByPrice rooms={roomsData}></FilterByPrice>
              </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-10 w-[1200px] mx-auto">
-                {
-                    roomsData.map((roomData,index)=><RoomCard key={index} roomData={roomData} ></RoomCard>)
-                }
-            </div>
+            
             
         </div>
     );
--- src/conponents/FilterByPrice/FilterByPrice.jsx
+++ src/conponents/FilterByPrice/FilterByPrice.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 
 import { useState } from "react";
+import RoomCard from "../RoomCard/RoomCard";
 
 const FilterByPrice = ({rooms}) => {
     const [sortBy, setSortBy] = useState('low-to-high');
@@ -8,20 +9,26 @@ const FilterByPrice = ({rooms}) => {
         setSortBy(e.target.value);
       };
       const sortedRooms = [...rooms];
-
+    //   console.log(sortedRooms);
       if (sortBy === 'low-to-high') {
         sortedRooms.sort((a, b) => a.price - b.price);
       } else if (sortBy === 'high-to-low') {
         sortedRooms.sort((a, b) => b.price - a.price);
       }
     return (
-         <div>
-      <div>
-        <label>Sort By:</label>
-        <select value={sortBy} onChange={handleSortChange}>
-          <option value="low-to-high">Low to High</option>
-          <option value="high-to-low">High to Low</option>
+    <div>
+      <div className="ml-20 flex items-center gap-2 mb-20">
+        <label className="text-black text-2xl  font-bold">Sort By:</label>
+        <select className="border border-black  rounded-lg px-8 py-2" value={sortBy} onChange={handleSortChange}>
+          <option className=" text-black font-bold" value="low-to-high"> Price Low to High</option>
+          <option value="" disabled></option>
+          <option className="font-bold text-black " value="high-to-low">Price High to Low</option>
         </select>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-10 w-[1200px] mx-auto">
+        {
+            sortedRooms.map((roomData,index)=><RoomCard key={index} roomData={roomData}></RoomCard>)
+        }
       </div>
       
     </div>
